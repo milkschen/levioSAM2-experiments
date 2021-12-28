f25dbe5fc2eaf3437af4f226af645be935c4e9f0
milkschen <s93rm6@gmail.com>
2021-12-28 03:44:17 -0500

    add a `tolerance` option

--- scripts/evaluate_mapping_correctness.py
+++ scripts/evaluate_mapping_correctness.py
@@ -1,12 +1,15 @@
 import pysam
 import sys
+
+TOLERANCE = 10
+
 gold = {}
 with pysam.AlignmentFile(sys.argv[1], 'r') as f:
     for r in f:
         if r.is_read1:
-            gold[r.query_name + '_1'] = r.reference_start
+            gold[r.query_name + '_1'] = (r.reference_name, r.reference_start)
         elif r.is_read2:
-            gold[r.query_name + '_2'] = r.reference_start
+            gold[r.query_name + '_2'] = (r.reference_name, r.reference_start)
 print(len(gold))
 
 cnt_correct = 0
@@ -18,10 +21,12 @@ with pysam.AlignmentFile(sys.argv[2], 'r') as f:
 
         cnt_all += 1
         if r.is_read1:
-            if gold[r.query_name + '_1'] == r.reference_start:
+            g = gold[r.query_name + '_1']
+            if g[0] == r.reference_name and abs(g[1] - r.reference_start) <= TOLERANCE:
                 cnt_correct += 1
         elif r.is_read2:
-            if gold[r.query_name + '_2'] == r.reference_start:
+            g = gold[r.query_name + '_2']
+            if g[0] == r.reference_name and abs(g[1] - r.reference_start) <= TOLERANCE:
                 cnt_correct += 1
 
 print(cnt_all)
